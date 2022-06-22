d572b72558560d2702fa31a60ab439dedc343403
romanyshyn97 <romanyshyn.ol.rom@gmail.com>
2022-06-22 19:06:17 +0300

    add middlewares to store and new functional actionCreators in action.js for filters and heroes

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,17 @@
+export const fetchHeroes = (request) => (dispatch) => {
+    dispatch(heroesFetching());
+    request("http://localhost:3001/heroes")
+        .then(data => dispatch(heroesFetched(data)))
+        .catch(() => dispatch(heroesFetchingError()))
+}
+
+export const fetchFilters = (request) => (dispatch) => {
+    dispatch(filtersFetching());
+    request("http://localhost:3001/filters")
+        .then(data => dispatch(filtersFetched(data)))
+        .catch(() => dispatch(filtersFetchingError()))
+}
+
 export const heroesFetching = () => {
     return {
         type: 'HEROES_FETCHING'
@@ -16,6 +30,8 @@ export const heroesFetchingError = () => {
         type: 'HEROES_FETCHING_ERROR'
     }
 }
+
+
 
 export const filtersFetching = () => {
     return {
